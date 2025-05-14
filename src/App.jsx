ecb978167e5b6c4d7fd5d38d6819193e84c66396
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-14 17:13:37 +0200

    [UPD]Add-functionapp-handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+      //stato iniziale per i campi del form
+      const[formData, setFormData]=useState({
+        author: "",
+        title: "",
+        body: "",
+        public: false,
+      });
+
+      //questa funzione aggiorna il form mentree lutente sta scrivendo
+      const handleChange=(e)=>{
+        const{name, value, type, checked}=e.value;
+        // se è una checkbox usa 'checked', altrimenti usa 'value'
+        setFormData((prev) => ({
+          ...prev,
+          [name]: type === "checkbox" ? checked : value,
+        }));
+      }
+
 
   return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   )
 }
